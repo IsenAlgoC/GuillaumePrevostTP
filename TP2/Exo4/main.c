9de8d3e7d3a8d6e93b5037aa395f4a5bcd1c3c79
PrevostGuillaume <prevost.guillaume@ymail.com>
2020-09-23 17:20:42 +0200

    push

---
+++ TP2/Exo4/main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main() {
+	srand(10);
+	int n = rand();
+	int a = -1;
+	int i = 0;
+
+	while(a!=n){
+		printf("Votre nombre : ");
+		scanf_s("%d", &a);
+		i++;
+		if (a > n) {
+			printf("Trop grand\n");
+		} 
+		if(a<n) {
+			printf("Trop petit\n");
+		}
+	}
+	printf("\nBravo !\nVotre score est de %d",i);
+}
