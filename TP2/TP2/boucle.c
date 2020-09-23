1d6df6aeb3558bbd8e7a5eaefbe6d3a34246f1b0
PrevostGuillaume <prevost.guillaume@ymail.com>
2020-09-23 15:47:09 +0200

    push

--- TP2/TP2/boucle.c
+++ TP2/TP2/boucle.c
@@ -2,20 +2,41 @@
 #include <stdio.h>
 
 
+int depassement(int number) {
+	int i = 0;
+	int sum = 0;
+	while (65535 - sum >= i) {
+		i += 1;
+		sum += i;
+	}	// i = 361 : dernière valeur possible avant dépassement
+	if (number > i) { 
+		return 1; 
+	} else {
+		return 0;
+	}
+}
 
-int n = 100;
-int sum = 0;
-int i = 0;
 
 int main() {
+	int n = 1000;
+	unsigned short int sum = 0;
+	int i = 0;
+
+	while (depassement(n)) {
+		printf("Entrer n : ");
+		scanf_s("%hu", &n);
+	} 
+
+
+	printf("valeur de n : %d", n);
 	for (i = 0; i <= n; i++) {
 		sum += i;
 	}
-	printf("sum for : %d", sum);
+	printf("\nsum for : %d", sum);
 
 	i = 0;
 	sum = 0;
-	while (i < n) {
+	while (i < n && 65535-sum >= i) {	// boucle modifiée pour eviter le dépassement
 		i += 1;
 		sum += i;
 	}
@@ -28,4 +49,7 @@ int main() {
 		sum += i;
 	} while (i < n);
 	printf("\nsum do while : %d", sum);
+
+
+
 }
