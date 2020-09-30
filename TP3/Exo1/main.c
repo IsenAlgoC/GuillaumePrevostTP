9b5f6b43a08b952f90c6cedad2a78dd41174de7e
Guillaume PREVOST <guillaume.prevost@isen.yncrea.fr>
2020-09-30 13:06:22 +0200

    push

--- TP3/Exo1/main.c
+++ TP3/Exo1/main.c
@@ -1,2 +1,12 @@
-int guigui = 0;
-int a = 0;
+#include <stdlib.h>
+#include <stdio.h>
+
+#define carre(x) (x) * (x)
+int main() {
+	int nb = 5;
+	printf("carre(%d) = %d", nb, carre(nb));
+
+	// Sans modification, carre(nb+1) renvoie 11
+	// Effectivement, 5 + 1*5 + 1 = 11  : Il faut mettre les parenthèses correctement.
+	printf("\ncarre(%d+1) = %d", nb, carre(nb+1));
+}
