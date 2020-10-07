54d1687fa29775cb14f97a73e0cd8243f24f9fbc
Guillaume PREVOST <guillaume.prevost@isen.yncrea.fr>
2020-10-07 17:13:34 +0200

    push

--- TP4/Exo4/main.c
+++ TP4/Exo4/main.c
@@ -7,7 +7,6 @@
 
 int main() {
 	char mot[20];
-	char tom[20];
 	char letters_min[] = "azertyuiopmlkjhgfdsqwxcvbn";
 	char letters_maj[] = "AZERTYUIOPMLKJHGFDSQWXCVBN";
 	int lengthMot = 0;
@@ -18,6 +17,8 @@ int main() {
 
 	lengthMot = strlen(mot);
 
+
+	// On met le mot en minuscules
 	int booleen = 0;
 	char letter;
 	for (int j = 0; j < lengthMot; j++) {
@@ -34,10 +35,40 @@ int main() {
 			mot[j] += 32;
 		}
 	}
+	
+	// On retire les caractères spéciaux
+	for (int j = 0; j < lengthMot; j++) {
+		letter = mot[j];
+		booleen = 0;
+		// On teste si la lettre est un caractère spécial
+		for (int i = 0; i <= sizeof(letters_min); i++) {
+			if (letters_min[i] == letter) {
+				booleen += 1;
+			}
+		}
+		// Si la lettre est un caractère spécial, on la change en -1
+		if (booleen == 0) {
+			mot[j] = -1;
+		}
+	}
+
+	// On retire les caracteres speciaux
+	char mot_[20];
+	int n = 0;
+	for (int j = 0; j < lengthMot+1; j++) {
+		if (mot[j] != -1) {
+			mot_[n] = mot[j];
+			n++;
+		}		
+	}
+	printf("\n%s", mot_);
+
+	// On teste la symetrie
+	lengthMot = strlen(mot_);
 
 	int test = 1;
 	for (int i = 0; i < lengthMot / 2; i++) {
-		if (mot[i] != mot[lengthMot - i - 1]) {
+		if (mot_[i] != mot_[lengthMot - i - 1]) {
 			test = 0;
 		}
 	}
